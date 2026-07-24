a2d324a02d8f45a744e789d1070ad7748a10f973
CDiangell-dei <salmoinfinyt@gmail.com>
2026-07-24 09:03:04 -0400

    Add files via upload

---
+++ service_worker_pwa.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'amazon-aco-almoxarifado-v1';
+const urlsToCache = [
+  './registro_luvas.html',
+  './dashboard.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
